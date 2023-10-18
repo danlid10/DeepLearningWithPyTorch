3e331ccea44d1d84d55fd016b42d6da365003fa4
Dan Lidayi <danlid10@gmail.com>
2023-10-18 16:56:57 +0300

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,21 +1,61 @@
 import torch
+import torch.nn as nn
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
-training_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(28),
-        transforms.RandomRotation(45),
-        #transforms.ToTensor()
-])
+""" The MNIST dataset consists of 60,000 training images and 10,000 testing images,
+with each image being a grayscale 28x28 pixel representation of a handwritten digit (0 through 9). """
 
-training_data = datasets.MNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=training_transforms
-)
+input_size = 28*28
+hidden_size = 100
+num_classes = 10
+learning_rate = 0.001
+num_epochs = 5
+batch_size = 80
 
-feature, label = training_data[4]
-print(label)
-plt.imshow(feature)
-plt.show()
+
+train_dataset = datasets.MNIST(root="data", train=True, download=True, transform=transforms.ToTensor())
+
+test_dataset = datasets.MNIST(root="data", train=False, transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size)
+
+
+model = nn.Sequential(nn.Linear(input_size, hidden_size), nn.ReLU(), nn.Linear(hidden_size, num_classes))
+criterion = nn.CrossEntropyLoss()
+optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Training the model
+total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (features, labels) in enumerate(train_loader):
+
+        features = features.reshape(-1, 28*28)
+        output = model(features)
+        loss = criterion(output, labels)
+
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+
+        if (i + 1) % 10 == 0:
+            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_steps}], Loss: {loss.item():.4f}')
+
+
+# Testing the model
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for features, labels in test_loader:
+        features = features.reshape(-1, 28*28)
+        outputs = model(features)
+
+        _, predicted = torch.max(outputs.data, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the {n_samples} test images: {acc} %')
+
