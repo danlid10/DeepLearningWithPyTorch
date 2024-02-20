1e7ea3165325e176418e80d5c26ee7c745b5a2b2
Dan Lidayi <danlid10@gmail.com>
2024-02-20 15:08:07 +0200

    Update CIFAR10_config.py

--- CIFAR10/CIFAR10_config.py
+++ CIFAR10/CIFAR10_config.py
@@ -7,7 +7,7 @@ with 50,000-image training set and a 10,000-image test set """
 # Parameters setup
 LEARNING_RATE = 0.001
 BATCH_SIZE = 256
-NUM_EPOCHS = 25
+NUM_EPOCHS = 40
 TRAIN_LOG_PATH = "CIFAR10_train_log.txt"
 TEST_LOG_PATH = "CIFAR10_test_log.txt"
 MODEL_PATH = "CIFAR10_model.pth"
@@ -39,7 +39,6 @@ class ConvNeuralNet(nn.Module):
         # init shape: n, 3, 32, 32
         x = self.pool(self.relu(self.conv1(x)))     # conv1: n, 6, 28, 28  -> pooling: n, 6, 14, 14
         x = self.pool(self.relu(self.conv2(x)))     # conv1: n, 16, 10, 10 -> poolong: n, 16, 5, 5
-        x = self.pool(self.relu(self.conv3(x)))     # conv1: n, 16, 10, 10 -> poolong: n, 16, 5, 5
         x = x.view(-1, 16 * 5 * 5)                  # flatten: n, 16*5*5 = 400
         x = self.relu(self.fc1(x))                  # fc1: n, 120
         x = self.relu(self.fc2(x))                  # fc2: n, 84
